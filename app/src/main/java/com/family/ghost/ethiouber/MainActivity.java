ff3ac00a5fb15ae623f5f8581f1c60cb69713d6c
RasabelGhost <abel.bekele9@gmail.com>
2018-06-02 03:31:37 -0700

    first commit

---
+++ app/src/main/java/com/family/ghost/ethiouber/MainActivity.java
@@ -0,0 +1,56 @@
+package com.family.ghost.ethiouber;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mDriver, mCustomer;
+
+    private VideoView mVideoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        mVideoView = (VideoView) findViewById(R.id.vid);
+
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.easytaxi);
+
+        mVideoView.setVideoURI(uri);
+        mVideoView.start();
+
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+            }
+        });
+
+
+
+        mDriver = (Button) findViewById(R.id.driver);
+        mCustomer = (Button) findViewById(R.id.customer);
+
+        startService(new Intent(MainActivity.this, onAppKilled.class));
+
+
+        mCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CustomerLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+}
